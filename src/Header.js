d8ae56d7b9a0c1fa86124fc42be9d6e805a10787
Doktorlich <sergei199522@mail.ru>
2022-04-18 21:50:01 +0300

    First commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/favicon.ico
    	new file:   public/index.html
    	new file:   public/logo192.png
    	new file:   public/logo512.png
    	new file:   public/manifest.json
    	new file:   public/robots.txt
    	new file:   src/App.css
    	new file:   src/App.js
    	new file:   src/App.test.js
    	new file:   src/Header.js
    	new file:   src/Tehnologies.js
    	new file:   src/index.css
    	new file:   src/index.js
    	new file:   src/logo.svg
    	new file:   src/reportWebVitals.js
    	new file:   src/setupTests.js

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+export default Header
+
+const Header = () => {
+    return( <div>
+        <a href="#">home</a><br/>
+        <a href="#">news feed</a><br/>
+        <a href="#">messages</a><br/>
+    </div>)
+};
---
+++ src/Tehnologies.js
@@ -0,0 +1,14 @@
+export default Tehnologies
+
+const Tehnologies = ()=>{
+    return (
+        <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+    )
+}
