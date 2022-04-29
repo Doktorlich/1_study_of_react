bd7a3419189e449b79df001d003c4e9d529edf6c
Doktorlich <sergei199522@mail.ru>
2022-04-29 18:15:33 +0300

    commit one

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header"
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
+
+const App = () => {
+    return (
+        <div className="app-wrapper">
+           <Header/> 
+            <NavBar/>
+           <Main/>
+        </div>
+    );
+};
+// https://olympus.crumina.net/activity/ - site pattern
+export default App;
