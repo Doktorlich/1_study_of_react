21b2fa6fb7f2c1d33c9fa843e1b539ebc8700e34
Doktorlich <sergei199522@mail.ru>
2022-05-07 21:23:42 +0300

    Change teg "a" to "NavLink"
    	Change attribute "href" in teg "a" to "to"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./NavBar.scss";
 
 
@@ -8,17 +9,17 @@ const NavBar = () => {
     return (
         <nav className="nav-bar">
             <ul className="list-icon">
-                <li className="list-icon__i-1 icons__style"><a href="/MembersNewsFeed"><img /></a></li>
-                <li className="list-icon__i-2 icons__style"><a href="/Activity"><img /></a></li>
-                <li className="list-icon__i-3 icons__style"><a href="/Members"><img /></a></li>
-                <li className="list-icon__i-4 icons__style"><a href="/Groups"><img /></a></li>
-                <li className="list-icon__i-5 icons__style"><a href="/Photos"><img /></a></li>
-                <li className="list-icon__i-6 icons__style"><a href="/Music"><img /></a></li>
-                <li className="list-icon__i-7 icons__style"><a href="/Video"><img /></a></li>
-                <li className="list-icon__i-8 icons__style"><a href="/Events"><img /></a></li>
-                <li className="list-icon__i-9 icons__style"><a href="/Forum"><img /></a></li>
-                <li className="list-icon__i-10 icons__style"><a href="/BlogGrid"><img /></a></li>
-                <li className="list-icon__i-11 icons__style"><a href="/Shop"><img /></a></li>
+                <li className="list-icon__i-1 icons__style"><NavLink to="/MembersNewsFeed"><img /></NavLink ></li>
+                <li className="list-icon__i-2 icons__style"><NavLink to="/Activity"><img /></NavLink></li>
+                <li className="list-icon__i-3 icons__style"><NavLink to="/Members"><img /></NavLink></li>
+                <li className="list-icon__i-4 icons__style"><NavLink to="/Groups"><img /></NavLink></li>
+                <li className="list-icon__i-5 icons__style"><NavLink to="/Photos"><img /></NavLink></li>
+                <li className="list-icon__i-6 icons__style"><NavLink to="/Music"><img /></NavLink></li>
+                <li className="list-icon__i-7 icons__style"><NavLink to="/Video"><img /></NavLink></li>
+                <li className="list-icon__i-8 icons__style"><NavLink to="/Events"><img /></NavLink></li>
+                <li className="list-icon__i-9 icons__style"><NavLink to="/Forum"><img /></NavLink></li>
+                <li className="list-icon__i-10 icons__style"><NavLink to="/BlogGrid"><img /></NavLink></li>
+                <li className="list-icon__i-11 icons__style"><NavLink to="/Shop"><img /></NavLink></li>
             </ul>
         </nav>
     );
