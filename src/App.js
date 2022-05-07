5693dbd2b631a221773c285537bd45fb412cade6
Doktorlich <sergei199522@mail.ru>
2022-05-07 20:44:05 +0300

    Added route in components folder "BodyPages"

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import MembersNewsFeed from "./components/BodyPages/MembersNewsFeed/MembersNewsFeed.jsx";
 import Footer from "./components/Footer/Footer";
 import Activity from "./components/BodyPages/Activity/Activity";
+import Members from "./components/BodyPages/Members/Members";
 import { Routes, Router, Switch, Route, Link } from "react-router-dom";
-
+import Groups from "./components/BodyPages/Groups/Groups"
+import Photos from "./components/BodyPages/Photos/Photos"
+import Music from "./components/BodyPages/Music/Music"
+import Video from "./components/BodyPages/Video/Video"
+import Events from "./components/BodyPages/Events/Events"
+import Forum from "./components/BodyPages/Forum/Forum"
+import BlogGrid from "./components/BodyPages/BlogGrid/BlogGrid"
+import Shop from "./components/BodyPages/Shop/Shop"
 const App = () => {
     return (
         <div className="app-wrapper">
@@ -16,9 +24,18 @@ const App = () => {
 
             <div className="app-wrapper-content">
                 <Routes>
-                    <Route path="*" element={<MembersNewsFeed />} />
                     <Route path="/MembersNewsFeed" element={<MembersNewsFeed />} />
                     <Route path="/Activity" element={<Activity />} />
+                    <Route path="/Members" element={<Members/>} />
+                    <Route path="/Groups" element={<Groups/>} />
+                    <Route path="/Photos" element={<Photos />} />
+                    <Route path="/Music" element={<Music/>} />
+                    <Route path="/Video" element={<Video/>} />
+                    <Route path="/Events" element={<Events/>} />
+                    <Route path="/Forum" element={<Forum/>} />
+                    <Route path="/BlogGrid" element={<BlogGrid/>} />
+                    <Route path="/Shop" element={<Shop/>} />
+                    
                 </Routes>
 
                 {/* <MembersNewsFeed />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,14 +10,15 @@ const NavBar = () => {
             <ul className="list-icon">
                 <li className="list-icon__i-1 icons__style"><a href="/MembersNewsFeed"><img /></a></li>
                 <li className="list-icon__i-2 icons__style"><a href="/Activity"><img /></a></li>
-                <li className="list-icon__i-3 icons__style"><a href=""><img /></a></li>
-                <li className="list-icon__i-4 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-5 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-6 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-7 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-8 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-9 icons__style"><a href="#"><img /></a></li>
-                <li className="list-icon__i-10 icons__style"><a href="#"><img /></a></li>
+                <li className="list-icon__i-3 icons__style"><a href="/Members"><img /></a></li>
+                <li className="list-icon__i-4 icons__style"><a href="/Groups"><img /></a></li>
+                <li className="list-icon__i-5 icons__style"><a href="/Photos"><img /></a></li>
+                <li className="list-icon__i-6 icons__style"><a href="/Music"><img /></a></li>
+                <li className="list-icon__i-7 icons__style"><a href="/Video"><img /></a></li>
+                <li className="list-icon__i-8 icons__style"><a href="/Events"><img /></a></li>
+                <li className="list-icon__i-9 icons__style"><a href="/Forum"><img /></a></li>
+                <li className="list-icon__i-10 icons__style"><a href="/BlogGrid"><img /></a></li>
+                <li className="list-icon__i-11 icons__style"><a href="/Shop"><img /></a></li>
             </ul>
         </nav>
     );
